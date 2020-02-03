c61a5b28e41b5f0c2bcf8e78f2136cf9b0e285c7
julesc35 <julersc5614029@gmail.com>
2020-02-03 13:00:17 +0100

    exercice 5

--- explo.c
+++ explo.c
@@ -12,12 +12,16 @@ struct lieux{
 typedef struct lieux lieu_t;
 
 int main(){
+int avantposte;
+int endurence = 100;
+int reste;
+int choix2;
 char passage1 [20] = "je rentre";
 char passage2 [20] = "je continue";
 char choix[20];
 
-
 printf("\n");
+printf("votre endurence est de %d\n", endurence);
 printf("vous arrivez devant un cheateau que faites-vous vous voulez rentrer [1] ou continuer votre chemin [2]?\n");
 printf("pour continuer le chemin taper: je rentre 	pour continuer son chemin, tapez: continuer\n");
 scanf("%s", &choix);
@@ -35,7 +39,7 @@ char lieu1 [20] = "chateau";
 char lieu2[20] = "caverne";
 char lieu3[20] = "marais";
 
-lieu_t chateau = {"chateau", "anciens lieux de pouvoir desormais en proie a la vegetation", 2, "caverne" && "marais"};
+lieu_t chateau = {"chateau", "anciens lieux de pouvoir desormais en proie a la vegetation", 4, "caverne" && "marais"};
 lieu_t caverne = {"caverne", "lieu de tenebre renfermant des créatures immondes", 8, "chateau" && "marais"};
 lieu_t marais = {"marais", "marais servant de cimetiere ou les corps erent", 10, "chateau"&&"caverne"};
 
@@ -46,19 +50,55 @@ printf("ou souhaitez vous aller ?\n");
 scanf("%s", &choix);
 
 if (strcmp(lieu1,choix)==0){
-printf("vous arrivez au %s, %s", chateau.nom, chateau.description);
-
+	int avantposte = 0;
+	printf("vous arrivez au %s, %s", chateau.nom, chateau.description);
+	endurence = endurence - chateau.difficultee + reste;
+	reste = chateau.difficultee;
+	printf("votre endurence: %d\n", endurence);
+	printf("le lieu est tranquille, voulez-vous vous reposer ou creer un avant poste ?\n");
+	printf("repos: 1	rien: 2		avant-poste: 3\n");
+	scanf("%d", &choix2);
+}
+else if(endurence <= chateau.difficultee){
+	printf("vous devez vous reposer\n");
 };
 
 if (strcmp(lieu2,choix)==0){
-printf("vous arrivez a la %s, %s", caverne.nom, caverne.description);
-
+	int avantposte = 0;
+	printf("votre endurence: %d\n", endurence);
+	printf("vous arrivez a la %s, %s", caverne.nom, caverne.description);
+	endurence = endurence - chateau.difficultee + reste;
+	reste = chateau.difficultee;
+	printf("voulez-vous creer un avant-poste ?\n");
+	scanf("%d", &choix2);
+}
+else if(endurence <= caverne.difficultee){
+	printf("vous devez vous reposer\n");
 };
 
 if (strcmp(lieu3,choix)==0){
-printf("vous arrivez au %s, %s", marais.nom, marais.description);
-
+	printf("votre endurence: %d\n", endurence);
+	printf("vous arrivez au %s, %s", marais.nom, marais.description);
+	endurence = endurence - chateau.difficultee + reste;
+	reste = chateau.difficultee;
+}
+else if(endurence <= marais.difficultee){
+	printf("vous devez vous reposer\n");
 };
+if (choix2 == 1){
+	endurence + 50;
+	printf("vous avez recuperer une partie de votre energie");
+};
+	if(endurence >= 100){
+		endurence = 100;
+}
+	else if(choix2 == 2){
+		printf("pas besoin de repos, un coup de potion et c'est parti !\n");
+};
+	//if (choix2 == 3){
+//avantposte = avantposte+ 1;
+//lieu.difficultee = lieu.difficultee/2;
+//	}
 
 return 0;
 }
